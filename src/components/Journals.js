ddc2ccb3e9abeca2aca0baf640eb49e0768cdda8
ingelieur <ingelieur@gmail.com>
2017-07-06 13:56:12 +0700

    journals start

---
+++ src/components/Journals.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+export default class Journals extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          HI! I'm your journals!
+        </Text>
+      </View>
+    )
+  }
+}
